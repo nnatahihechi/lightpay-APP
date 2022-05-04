ef97b7d6ca8ce2e244e4ff7aff1b8159a4de31ae
brosj <joshuaonyeuche@gmail.com>
2022-05-04 23:16:30 +0100

    Initial Setup 2

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import {Routes, Route} from 'react-router-dom'
+
+function App() {
+  return (
+    <Routes>
+      <Route>
+
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
