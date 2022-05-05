2602bc1c3b2b0e5f574886cbb4ec65a5f1d6f5fb
Achionyinye <achionyinyegiftbarbie@gmail.com>
2022-05-05 16:03:44 +0100

    verify branch

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,12 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import createError from 'http-errors'
+import express, {Request, Response, NextFunction} from 'express'
+import path from 'path';
+import cookieParser from 'cookie-parser'
+import logger from 'morgan'
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
+const indexRouter = require ("./routes/index")
+const usersRouter = require ("./routes/users")
 
 var app = express();
 
@@ -23,7 +24,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function(req:Request, res:Response, next:NextFunction) {
   next(createError(404));
 });
 
@@ -38,4 +39,5 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+
+export default app;
