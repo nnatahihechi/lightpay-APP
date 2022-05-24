233e7a298ff18804da08eadabe42a69143cc9161
brosj <joshuaonyeuche@gmail.com>
2022-05-24 22:00:04 +0100

    Return user's wallets

---
+++ server/src/controllers/getUserWallet.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express';
+import { promisify } from 'util';
+import pool from '../db/connection';
+import jwt from 'jsonwebtoken'
+require('dotenv').config();
+
+
+const getUserWallet = async (req: Request, res: Response) => {
+    const id  = req.user.id
+  try {
+    const myWallet = await pool.query(`SELECT address FROM "Wallets" WHERE "UserId"=${id}`);
+    
+    res.status(200).json(myWallet.rows);
+  } catch (error: any) {
+      res.status(500).json(error)
+    console.log(error.message);
+  }
+};
+
+export default getUserWallet;
