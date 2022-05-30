a621b99321c970280a890f7dda39c80f27527c4d
tnk <chinemelum.a@decagonhq.com>
2022-05-30 20:20:00 +0100

    added balance

--- server/src/controllers/wallets.controller.ts
+++ server/src/controllers/wallets.controller.ts
@@ -2,22 +2,23 @@ import { Request, Response } from 'express';
 import pool from '../db/connection';
 const web3 = require('web3');
 import dotenv from "dotenv";
+import Web3 from 'web3';
 
 dotenv.config();
 
 export const getBalances = async (req: Request | any, res: Response) => {
-
+  const web3 = new Web3(process.env.BSC_TESTNET_NODE!)
   const id = req.user.id;
   const coin = req.params.coin;
   const myWallet = await pool.query(
-    `SELECT address FROM "Wallets" WHERE UserId=${id} AND coin="${coin}"`
+    `SELECT address FROM "Wallets" WHERE "UserId"=${id} AND "coin"='${coin}'`
   );
   const address = myWallet.rows[0].address;
   try {
 
-    let balance = await web3.eth.getBalance(address)
+    let balance:any = await web3.eth.getBalance(address);
     let decimal = (process.env.BSC_TOKEN_DECIMAL! as unknown  as  number)
-    balance = balance/decimal;
+    balance = (balance as unknown as number) /decimal;
     return res.status(200).json({
       balance
     })
